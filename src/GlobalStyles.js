9b45aff6d0ba81153f00005308ade000fcfae8bf
Lukas Kreidenweis <lukas@deinklub.de>
2019-10-17 18:10:01 +0200

    modified global stylings / themes

--- src/GlobalStyles.js
+++ src/GlobalStyles.js
@@ -12,6 +12,20 @@ html {
   font-family: 'Cairo', sans-serif;
   font-size: 16px;
   font-weight: 400;
+  color: ${props => props.theme.main}
+}
+
+h1 {
+  font-size: 2em;
+}
+
+h2 {
+  font-size: 1.2em;
+}
+
+h3 {
+  font-size: 1em;
+  line-height: 1.2em;
 }
 
 button, input, select, textarea {
