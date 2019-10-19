2208788a226b60fb90125ae0e5cf0901f4011797
Lukas Kreidenweis <lukas@deinklub.de>
2019-10-19 14:27:39 +0200

    modified namings

--- src/components/MenuBurger.js
+++ src/components/MenuBurger.js
@@ -1,15 +1,12 @@
 import React from "react";
-import styled from "styled-components";
 import PropTypes from "prop-types";
 import { UIElementIconTop } from "./UIElement";
 import MenuBurgerIcon from "../icons/MenuBurgerIcon";
 
-const AnimatedMenuBurgerIcon = styled(MenuBurgerIcon)``;
-
 export default function MenuBurger({ open, setOpen }) {
   return (
     <UIElementIconTop open={open} onClick={() => setOpen(!open)}>
-      <AnimatedMenuBurgerIcon open={open} />
+      <MenuBurgerIcon />
       menu
     </UIElementIconTop>
   );
