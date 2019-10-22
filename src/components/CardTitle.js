4cfa89d85d6d0ebf75e98572ec54d1c7871f55a7
Lukas Kreidenweis <54030139+webdevelukas@users.noreply.github.com>
2019-10-22 12:28:30 +0200

    Card component (#32)
    * add json server / fake-api
    
    * add sample data structure in the api folder
    
    * add functionality to the gallery component
    
    * changed variables in the card component and found an easier way to write components with no functionality

--- src/components/CardTitle.js
+++ src/components/CardTitle.js
@@ -1,16 +1,8 @@
-import React from "react";
 import styled from "styled-components";
-import PropTypes from "prop-types";
 
-const StyledCardTitle = styled.h3`
+const CardTitle = styled.h3`
   grid-area: CardTitle;
   margin: 0;
 `;
 
-export default function CardTitle({ children }) {
-  return <StyledCardTitle>{children}</StyledCardTitle>;
-}
-
-CardTitle.propTypes = {
-  children: PropTypes.string
-};
+export default CardTitle;
