5ac441b144f4340110b074681316c12c35d8ec35
Lukas Kreidenweis <54030139+webdevelukas@users.noreply.github.com>
2019-10-23 14:23:41 +0200

    add Loginpage (#39)
    * add login page
    
    * changed margin of button

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import React from "react";
 import { ThemeProvider } from "styled-components";
 import defaultTheme from "./themes/default";
 import GlobalStyles from "./GlobalStyles";
-import NewArticle from "./pages/NewArticle";
+import Login from "./pages/Login";
 
 function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyles />
-      <NewArticle />
+      <Login />
     </ThemeProvider>
   );
 }
--- src/components/MainArea.js
+++ src/components/MainArea.js
@@ -8,4 +8,23 @@ const MainArea = styled.main`
   margin: 1rem 0 3rem;
 `;
 
+export const ColoredMainArea = styled(MainArea)`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background: ${({ bgColor }) => bgColor};
+  color: ${({ theme }) => theme.highlightContrast};
+  height: 100vh;
+  padding: 3rem 3rem 2rem;
+  margin: 0;
+
+  h2 {
+    align-self: flex-start;
+    font-size: 1em;
+    text-align: left;
+    text-transform: none;
+    margin: 1em 0 0.25em 0.5em;
+  }
+`;
+
 export default MainArea;
