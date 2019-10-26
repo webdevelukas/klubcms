8f22c63f9c9afde4c98e9500cb7d3a712f8dfbfe
Lukas Kreidenweis <54030139+webdevelukas@users.noreply.github.com>
2019-10-26 20:41:23 +0200

    add form, dropdown and label. Modified Textarea and input (#48)

---
+++ src/components/Form.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+`;
+
+export default Form;
