bf7723b6cea9a0ffce2563bd54a4f58521025fd0
Lukas Kreidenweis <lukas@deinklub.de>
2019-10-27 16:10:21 +0100

    add waiting on data to edit article page

--- src/pages/EditArticlePage.js
+++ src/pages/EditArticlePage.js
@@ -1,8 +1,7 @@
 // Imported dependencies / functions
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import { getArticle, getEvents } from "../api/fetch";
-import { articleTemplate } from "../api/articleTemplate";
 
 // Imported components / icons
 import MainArea from "../components/MainArea";
@@ -20,61 +19,66 @@ export default function EditArticlePage({ match }) {
     params: { articleId }
   } = match;
 
-  const [article, setArticle] = useState(articleTemplate); // Can I use mongoose for this template?
+  const [article, setArticle] = useState(false);
   const [events, setEvents] = useState([]);
-
-  React.useEffect(() => {
+  useEffect(() => {
     getArticle(articleId).then(fetchedArticle => setArticle(fetchedArticle));
 
     getEvents().then(fetchedEvents => {
       setEvents(fetchedEvents);
     });
-  }, [articleId]);
-
+    // eslint-disable-next-line
+  }, []);
   return (
     <>
       <Menu />
-      <MainArea>
-        <h1>What do you want to edit?</h1>
-        <hr />
-        <FunctionBar>
-          <div>
-            Date added: <b>{article.date.added}</b>
-          </div>
-          <div>
-            Date updated: <b>{article.date.updated}</b>
-          </div>
-        </FunctionBar>
-        <h2>Event</h2>
-        <Form>
-          <DropdownFullWidth value={article.eventId}>
-            {events.map(event => {
-              return (
-                <option key={event.id} value={event.id}>
-                  {event.date} :: {event.name}
-                </option>
-              );
-            })}
-          </DropdownFullWidth>
-          <h2>Title</h2>
-          <TextareaWithBoldText
-            placeholder="What are you talking about?"
-            defaultValue={article.title}
-          />
-          <h2>Text</h2>
-          <Textarea
-            placeholder="What you wanna tell the people out there?"
-            defaultValue={article.content}
-          />
-          <h2>Photos</h2>
-          <Gallery articleGallery={article.media.images.gallery} />
-
-          <Input type="file" multiple />
-
+      {article ? (
+        <MainArea>
+          <h1>What do you want to edit?</h1>
           <hr />
-          <Button>Save Article</Button>
-        </Form>
-      </MainArea>
+
+          <FunctionBar>
+            <div>
+              Date added: <b>{article.date.added}</b>
+            </div>
+            <div>
+              Date updated: <b>{article.date.updated}</b>
+            </div>
+          </FunctionBar>
+
+          <Form>
+            <h2>Event</h2>
+            <DropdownFullWidth defaultValue={article.eventId}>
+              {events.map(event => {
+                return (
+                  <option key={event.id} name={event.name} value={event.id}>
+                    {event.date} :: {event.name}
+                  </option>
+                );
+              })}
+            </DropdownFullWidth>
+            <h2>Title</h2>
+            <TextareaWithBoldText
+              placeholder="What are you talking about?"
+              defaultValue={article.title}
+            />
+            <h2>Text</h2>
+            <Textarea
+              placeholder="What you wanna tell the people out there?"
+              defaultValue={article.content}
+            />
+            <h2>Photos</h2>
+            <Gallery articleGallery={article.media.images.gallery} />
+
+            <Input type="file" multiple />
+
+            <hr />
+            <Button>Save Article</Button>
+          </Form>
+        </MainArea>
+      ) : (
+        "Loading Content"
+      )}
     </>
   );
 }
